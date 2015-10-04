b8506ef5243b7357b72d93768afcb184ec2d6df2
tripvi <viktriom@gmail.com>
2015-10-04 20:20:07 +0530

    Created the initial project structure.

---
+++ sporol/src/main/java/com/incipience/sporol/base/controllers/BaseController.java
@@ -0,0 +1,20 @@
+package com.incipience.sporol.base.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by tripvi on 04/10/15.
+ */
+
+@Controller
+public class BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    public String orphanRequest(){
+        String retVal=null;
+
+        return retVal;
+    }
+}
---
+++ sporol/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+well this seems to be working.
+</body>
+</html>
